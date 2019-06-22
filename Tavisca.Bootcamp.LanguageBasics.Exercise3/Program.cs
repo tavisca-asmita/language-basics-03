3dbc2113406b9c80161851c05ca999d796277a1a
tavisca-asmita <51910971+tavisca-asmita@users.noreply.github.com>
2019-06-22 23:19:43 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -41,7 +41,171 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
             // Add your code here.
+            int[] calorie = new int[fat.Length];
+            int[] meal = new int[dietPlans.Length];
+            
+            for (int i = 0; i < fat.Length; i++)
+            {
+                calorie[i] = protein[i]*5 + carbs[i] * 5 + fat[i] * 9;
+            }
+            
+            for (int i = 0; i < dietPlans.Length; i++)
+            {
+                List<int> al = new List<int>();
+                List<int> l = new List<int>();
+
+                foreach (char x in dietPlans[i])
+                {
+                    
+                    if (al.Count() == 0)
+                    {
+                        switch (x)
+                        {
+                            case 'P':
+                                al = Max(protein);
+                                break;
+                            case 'F':
+                                al = Max(fat);
+                                break;
+                            case 'C':
+                                al = Max(carbs);
+                                break;
+                            case 'T':
+                                al = Max(calorie);
+                                break;
+                            case 'p':
+                                al = Min(protein);
+                                break;
+                            case 'f':
+                                al = Min(fat);
+                                break;
+                            case 'c':
+                                al = Min(carbs);
+                                break;
+                            case 't':
+                                al = Min(calorie);
+                                break;                            
+                            default :                                
+                                break;
+                        }
+                    }
+
+                    else
+                    {
+                        switch (x)
+                        {
+                            case 'P':
+                                al = Tmax(protein,al);
+                                break;
+                            case 'F':
+                                al = Tmax(fat,al);
+                                break;
+                            case 'C':
+                                al = Tmax(carbs,al);
+                                break;
+                            case 'T':
+                                al = Tmax(calorie,al);
+                                break;
+                            case 'p':
+                                al = Tmin(protein,al);
+                                break;
+                            case 'f':
+                                al = Tmin(fat,al);
+                                break;
+                            case 'c':
+                                al = Tmin(carbs,al);
+                                break;
+                            case 't':
+                                al = Tmin(calorie,al);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    
+                    int a = al.Count();
+                                                            
+                    if (a > 0)
+                    {
+                        meal[i] = al[0];
+                        l.Add(al[0]);
+                    }
+                       
+                    else
+                        meal[i] = l[0];
+                }
+            }
+            
+            return meal;
             throw new NotImplementedException();
+        }
+
+        public static List<int> Min(int[] a)
+        {
+            int min = 999;
+            List<int> l = new List<int>();
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < min)
+                    min = a[i];             
+            }
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == min)
+                    l.Add(i);
+            }
+            return l;
+        }
+
+        public static List<int> Max(int[] a)
+        {
+            int max = -999;
+            List<int> l = new List<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                    max = a[i];
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == max)
+                    l.Add(i);
+            }
+            return l;
+        }
+
+        public static List<int> Tmin(int[] a, List<int> b)
+        {
+            int min = 999;
+            List<int> l = new List<int>();
+            foreach (int i in b)
+            {
+                if (a[i] < min)
+                    min = a[i];
+            }
+            foreach (int i in b)
+            {
+                if (a[i] == min)
+                    l.Add(i);
+            }
+            return l;
+        }
+
+        public static List<int> Tmax(int[] a, List<int> b)
+        {
+            int max = -999;
+            List<int> l = new List<int>();
+            foreach (int i in b)
+            {
+                if (a[i] > max)
+                    max = a[i];
+            }
+            foreach (int i in b)
+            {
+                if (a[i] == max)
+                    l.Add(i);
+            }
+            return l;
         }
     }
 }
